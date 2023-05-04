c16aebc0677e8369cdde6838883f1642a7131ffa
Connor Dillon <connor@codewithconnor.com>
2023-05-03 21:04:15 -0700

    :construction: WIP

--- indexDev.js
+++ indexDev.js
@@ -1,30 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  
+
+  chrome.storage.local.set({
+  'key1': 'value',    
+    'key2': 'value',
+    'key3': 'value',
+    'key3': 'value',
+    'key4': 'value',
+    'key5': 'value',
+  'key6': 'value'
+  });
+  chrome.storage.local.set({ key: value }).then(() => {
+  console.log("Value is set to " + value);
+chrome.storage.local.get(["key"]).then((result) => {
+  console.log("Value currently is " + result.key);
+});
+
   chrome.action.onClicked.addListener((tab) => {
-    alert('good')
+    console.log('clicked')
   });
   // get textarea div
   //add eventlistener to textareaelement
   //function should set item to local storage
   let cache = {};
   const note = document.querySelector('#note')
-  note.style = `background: blue;`
   note.addEventListener("change", function (event) {
+    console.log('note change event dispatched')
     cache = event.target.checked;
-    chrome.storage.local.set({cache});
+    
 
     const key = window.location.toString()
     //console.log(key);
     const value = 'textarea\ncontent'
 
-    chrome.storage.local.set({ key: value }).then(() => {
-  console.log("Value is set to " + value);
+    
 });
 
-chrome.storage.local.get(["key"]).then((result) => {
-  console.log("Value currently is " + result.key);
-});
+
 
   })
 });
